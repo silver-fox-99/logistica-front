23d3cfa5879eabc53981583adbe24fb8923d102b
silver-fox-99 <silverfox99m@gmail.com>
2025-10-23 10:28:07 +0300

    hide filter data

--- src/widgets/public/PublicFiltersDrawer.tsx
+++ src/widgets/public/PublicFiltersDrawer.tsx
@@ -234,12 +234,12 @@ export function PublicFiltersDrawer({ open, initial, onClose, onApply, kind }: P
                         disabled={dropoffCitiesDisabled}
                     />
 
-                    <Stack direction="row" gap={1.2}>
-                        <TextField fullWidth size="small" type="date" label="From" InputLabelProps={{ shrink: true }}
-                                   value={f.dropoff_date_from ?? ""} onChange={(e)=>setF(v=>({...v,dropoff_date_from:e.target.value||undefined}))}/>
-                        <TextField fullWidth size="small" type="date" label="To" InputLabelProps={{ shrink: true }}
-                                   value={f.dropoff_date_to ?? ""} onChange={(e)=>setF(v=>({...v,dropoff_date_to:e.target.value||undefined}))}/>
-                    </Stack>
+                    {/*<Stack direction="row" gap={1.2}>*/}
+                    {/*    <TextField fullWidth size="small" type="date" label="From" InputLabelProps={{ shrink: true }}*/}
+                    {/*               value={f.dropoff_date_from ?? ""} onChange={(e)=>setF(v=>({...v,dropoff_date_from:e.target.value||undefined}))}/>*/}
+                    {/*    <TextField fullWidth size="small" type="date" label="To" InputLabelProps={{ shrink: true }}*/}
+                    {/*               value={f.dropoff_date_to ?? ""} onChange={(e)=>setF(v=>({...v,dropoff_date_to:e.target.value||undefined}))}/>*/}
+                    {/*</Stack>*/}
                 </Stack>
 
                 {/* VEHICLE */}
