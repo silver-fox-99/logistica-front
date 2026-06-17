d51e04bd3b4bf095c3ceafef3d470d1d226857de
silver-fox-99 <silverfox99m@gmail.com>
2026-06-17 12:41:51 +0300

    socket update

--- src/features/notifications/websocket/useNotificationsWebSocket.ts
+++ src/features/notifications/websocket/useNotificationsWebSocket.ts
@@ -18,6 +18,7 @@ export function useNotificationsWebSocket() {
             auth: {
                 token: refreshToken,
             },
+            transports: ["websocket"],
         });
 
         socketRef.current = socket;
